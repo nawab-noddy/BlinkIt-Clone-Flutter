3976a6ac6cd995597f24dc8ad12f8327578ac04d
Mohammad Anas <mohammadanas8948@gmail.com>
2026-01-07 16:01:16 +0530

    update screencart

--- lib/repository/screens/cart/cartscreen.dart
+++ lib/repository/screens/cart/cartscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/repository/widgets/uihelper.dart';
 
 class Cartscreen extends StatelessWidget {
   const Cartscreen({super.key});
@@ -6,7 +7,51 @@ class Cartscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("CartScreen"),),
+      body: Column(
+        children: [
+          SizedBox(height: 40),
+          Container(
+            height: 160,
+            width: double.infinity,
+            color: Color(0XFFF7CB45),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    SizedBox(width: 10),
+                    UiHelper.Customtext(
+                      text: "Blinkit in",
+                      color: Color(0XFF000000),
+                      fontweight: FontWeight.bold,
+                      fontsize: 14,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(width: 10),
+                        UiHelper.Customtext(
+                          text: "16 minutes",
+                          color: Color(0XFF000000),
+                          fontweight: FontWeight.bold,
+                          fontsize: 20,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(children: [ // continue from here vid timing - 1:18:10
+                  UiHelper.Customtext(
+                    text: "16 minutes",
+                    color: Color(0XFF000000),
+                    fontweight: FontWeight.bold,
+                    fontsize: 20,
+                  ),
+                ],)
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
