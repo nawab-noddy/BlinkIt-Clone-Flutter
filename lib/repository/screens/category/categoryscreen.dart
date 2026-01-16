67db895f5ec3d07a68d7305e393a8d06e90f1051
Mohammad Anas <mohammadanas8948@gmail.com>
2026-01-16 15:42:40 +0530

    Upating and Creating Category Screen

--- lib/repository/screens/category/categoryscreen.dart
+++ lib/repository/screens/category/categoryscreen.dart
@@ -1,12 +1,97 @@
 import 'package:flutter/material.dart';
 
-class CategoryScreen extends StatelessWidget {
-  const CategoryScreen({super.key});
+import '../../widgets/uihelper.dart';
 
+class CategoryScreen extends StatelessWidget {
+  TextEditingController searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("CategoryScreen"),),
+      body: Column(
+        children: [
+          SizedBox(height: 40),
+          Stack(
+            children: [
+              Container(
+                height: 190,
+                width: double.infinity,
+                color: Color(0XFFF7CB45),
+                child: Column(
+                  children: [
+                    SizedBox(height: 30),
+                    Row(
+                      children: [
+                        SizedBox(width: 20),
+                        UiHelper.Customtext(
+                          text: "Blinkit in",
+                          color: Color(0XFF000000),
+                          fontweight: FontWeight.bold,
+                          fontsize: 15,
+                          fontfamily: "bold",
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(width: 20),
+                            UiHelper.Customtext(
+                              text: "16 minutes",
+                              color: Color(0XFF000000),
+                              fontweight: FontWeight.bold,
+                              fontsize: 20,
+                              fontfamily: "bold",
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(width: 20),
+                        UiHelper.Customtext(
+                          text: "Home ",
+                          color: Color(0XFF000000),
+                          fontweight: FontWeight.bold,
+                          fontsize: 14,
+                        ),
+                        UiHelper.Customtext(
+                          text: "- Indira Nagar, Lucknow",
+                          color: Color(0XFF000000),
+                          fontweight: FontWeight.bold,
+                          fontsize: 14,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                right: 20,
+                bottom: 100,
+                child: CircleAvatar(
+                  radius: 15,
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.person, color: Colors.black, size: 20),
+                ),
+              ),
+              Positioned(
+                bottom: 30,
+                left: 20,
+                child: UiHelper.CustomTextField(cotroller: searchController),
+              ),
+            ],
+          ),
+          SizedBox(height: 30,),
+          Row(children: [
+            SizedBox(width: 20,),
+            UiHelper.Customtext(
+              text: "Grocery & Kitchen",
+              color: Color(0XFF000000),
+              fontweight: FontWeight.bold,
+              fontsize: 14,
+              fontfamily: "bold",
+            ),
+          ],)
+        ],
+      ),
     );
   }
 }
