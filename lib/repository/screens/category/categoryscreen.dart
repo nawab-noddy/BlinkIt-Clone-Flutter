796464071d0710e685b90a5d86b0f794f9146cf0
Mohammad Anas <mohammadanas8948@gmail.com>
2026-01-16 16:08:56 +0530

    Upating and Creating Category Screen

--- lib/repository/screens/category/categoryscreen.dart
+++ lib/repository/screens/category/categoryscreen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import '../../widgets/uihelper.dart';
 
 class CategoryScreen extends StatelessWidget {
   TextEditingController searchController = TextEditingController();
+  var groceryKitchen = [
+    {"img": "image 41.png", "text": "Vegetables \n& Fruit"},
+    {"img": "image 42.png", "text": "Atta, Dal & \nRice"},
+    {"img": "image 43.png", "text": "Oil, Ghee & \n Masala"},
+    {"img": "image 44 (1).png", "text": "Dairy, Bread & \nMilk"},
+    {"img": "image 45 (1).png", "text": "Biscuits & \nBakery"},
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,17 +87,39 @@ class CategoryScreen extends StatelessWidget {
               ),
             ],
           ),
-          SizedBox(height: 30,),
-          Row(children: [
-            SizedBox(width: 20,),
-            UiHelper.Customtext(
-              text: "Grocery & Kitchen",
-              color: Color(0XFF000000),
-              fontweight: FontWeight.bold,
-              fontsize: 14,
-              fontfamily: "bold",
+          SizedBox(height: 30),
+          Row(
+            children: [
+              SizedBox(width: 20),
+              UiHelper.Customtext(
+                text: "Grocery & Kitchen",
+                color: Color(0XFF000000),
+                fontweight: FontWeight.bold,
+                fontsize: 14,
+                fontfamily: "bold",
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 78,
+                  width: 71,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0XFFD9EBEB),
+                  ),
+                  child: UiHelper.CustomImage(
+                    img: groceryKitchen[index]["img"].toString(),
+                  ),
+                );
+              },
+              itemCount: groceryKitchen.length,
+              scrollDirection: Axis.horizontal,
             ),
-          ],)
+          ),
         ],
       ),
     );
